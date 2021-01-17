e8c80b6c9d3f7f931fdedf69624224f26e9b03a8
cespino93 <chrisl.espino@gmail.com>
2021-01-17 16:58:57 -0500

    add recipe model and migration using corneal, and associations'

---
+++ db/migrate/20210117214644_create_recipes.rb
@@ -0,0 +1,10 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :content
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
