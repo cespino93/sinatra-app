16eed3493467f5897ec2bad0e847ba9948e8b705
cespino93 <chrisl.espino@gmail.com>
2021-01-19 21:35:58 -0500

    added signup/logout screens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,3 +25,4 @@ class ApplicationController < Sinatra::Base
   end
 
 end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,11 +29,50 @@ class UsersController < ApplicationController
       
       # what routes do I need for signup?
       get '/signup' do
-    
+      # erb (render) a view
+        erb :signup
+    end
+      
+    post '/users' do
+      #here is where we will create a new user and persist the
+      #user to the database
+      #params will look like this:
+      # {
+      # "name"=>"Maria",
+      # "email"=>"maria@m.com",
+      # "password"=>"password"
+      # }
+      # I only want to persist a user that has a name, email, 
+      # AND password
+      if params[:name] != "" && params[:email] != "" &&
+      params[:password] != ""
+        # valid input
+        @user = User.create(params)
+        session[:user_id] = @user.id # actually logging the user in
+        # where do i go now?
+        # lets go to the user show page
+        redirect "/users/#{@user.id}"
+    else
+      # not valid input
+      # it would be better to include a message to the user
+      # telling them what is wrong
+        redirect '/signup'
+      end
     end
 
     # user SHOW route
-    get '/users/:id' do
-      "this will be the user show route"
-end  
+    get '/users/:slug' do
+      # what do I need to do first?
+      @user = User.find_by(id: params[:id])
+
+      erb :'/users/show'
+    end  
+
+    get '/logout' do
+      session.clear
+      redirect '/'
+    end
+
+
+
 end
